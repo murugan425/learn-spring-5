5604369b6731f1567e0c58fd676c4563465e005c
Murugan Nagarajan <murugan425@gmail.com>
2017-09-18 20:16:01 +0800

    "Created this sub-project 'learnspringdi' for Spring 5 Dependency Injection Concepts"
    Signed-off-by: Murugan Nagarajan <murugan425@gmail.com>

---
+++ learnspringdi/src/main/java/tamil/learn/springframework/learnspringdi/LearnspringdiApplication.java
@@ -0,0 +1,12 @@
+package tamil.learn.springframework.learnspringdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnspringdiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnspringdiApplication.class, args);
+	}
+}
