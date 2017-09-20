532a6f7269435509998f980b54c3349b61a4f2a4
Murugan Nagarajan <murugan425@gmail.com>
2017-09-21 02:07:39 +0800

    Demo App created to apply all the DI concepts in a WebApp.
    Signed-off-by: Murugan Nagarajan <murugan425@gmail.com>

---
+++ learnspringdiapp/src/main/java/tamil/learn/springframework/learnspringdiapp/services/JokeServiceImpl.java
@@ -0,0 +1,20 @@
+/* Created by Murugan_Nagarajan on 9/21/2017 */
+package tamil.learn.springframework.learnspringdiapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
