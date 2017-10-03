dc4f768b8023c20e24c316bcb2b2e9cd656a1d0a
Murugan Nagarajan <murugan425@gmail.com>
2017-10-04 07:37:21 +0800

    Spring JUNIT Implementations - Controller Test class implementation. Refactored the controller URL in such a way it is not equal to the view name returned/html file name to be displayed.
    Signed-off-by: Murugan Nagarajan <murugan425@gmail.com>

--- learnspringrecipeapp/src/main/java/tamil/learn/springframework/learnspringrecipeapp/bootstrap/RecipeBootstrap.java
+++ learnspringrecipeapp/src/main/java/tamil/learn/springframework/learnspringrecipeapp/bootstrap/RecipeBootstrap.java
@@ -96,6 +96,60 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 
         greenCorianderRecipe.getCategories().add(categoryRepository.findByCategoryname("Indian").get());
 
+
+        Recipe chickenBiryani = new Recipe();
+        chickenBiryani.setDescription("Biryani Rice Recipe");
+        chickenBiryani.setCookTime(60);
+        chickenBiryani.setPrepTime(20);
+        chickenBiryani.setDifficulty(Difficulty.HARD);
+        chickenBiryani.setDirections("Step 1"
+                + " First, in order to marinate the chicken, take a large bowl, put greek yogurt, turmeric chili powder" +
+                " along with salt according one's taste. Then, add the chicken thighs in the mixture and keep aside for " +
+                "about 20-30 minutes so that the yogurt mixture is properly absorbed by the chicken. Also, soak saffron " +
+                "in the milk and keep aside. "
+                + "Step 2"
+                + "In the meanwhile, pour refined oil in a deep-bottomed pan, keeping it on medium flame. " +
+                "Fry cumin seeds and green cardamom for about 2 minutes. Once done, immediately add the sliced onion " +
+                "and fry for 15 minutes straight. Make sure you don't burn it, so when the onion starts to get brown " +
+                "in color, add tomatoes and tomato puree and fry for another 5 minutes."
+                + "Step 3"
+                + "Next, add the slit green chilies to the mixture along with ginger-garlic paste, frying the mixture " +
+                "yet again for a minute. Then, add coriander powder, turn the flame to medium low, add the marinated " +
+                "chicken and mix for a while so that the ingredients absorb the juices properly."
+                + "Step 4"
+                + "Turn the flame to medium again and heat-through for about 5-6 minutes only to turn it over to low " +
+                "heat. Cover with a lid and let simmer for 5 minutes. Make sure to keep stirring during the entire " +
+                "process, else the chicken might stick to the bottom, eventually ending up burnt. You can add little " +
+                "water if you find the consistency too thick."
+                + "Step 5"
+                + "Once done, turn off the flame, take the boiled rice, putting half of its quantity in the pan, " +
+                "sprinkle milk soaked saffron along with garam masala, mint and coriander. Put the remaining rice over " +
+                "and garnish with the same mentioned four ingredients."
+                + "Step 6"
+                + "Lastly, cover the lid, turn the flame to low medium and let simmer for about 5 minutes. Once done, " +
+                "put it off and let the biryani stay covered for about another 10 minutes. Serve hot, along with raita" +
+                " or any chutney of your choice.");
+
+
+        chickenBiryani.addIngredient(new Ingredient("Boiled Basmati rice", new BigDecimal(3), cupUOMOptional.get()));
+        chickenBiryani.addIngredient(new Ingredient("Minced Mint Leaves", new BigDecimal(4), teaSpoonUOMOptional.get()));
+        chickenBiryani.addIngredient(new Ingredient("Onion", new BigDecimal(2), largeUOMOptional.get()));
+        chickenBiryani.addIngredient(new Ingredient("Ginger Garlic Paste", new BigDecimal(1), tableSpoonUOMOptional.get()));
+        chickenBiryani.addIngredient(new Ingredient("Chicken Thighs", new BigDecimal(1), ounceUOMOptional.get()));
+        chickenBiryani.addIngredient(new Ingredient("Garam Masala", new BigDecimal(1), teaSpoonUOMOptional.get()));
+        chickenBiryani.addIngredient(new Ingredient("Cumin Seeds", new BigDecimal(0.5), teaSpoonUOMOptional.get()));
+        chickenBiryani.addIngredient(new Ingredient("Olive Oil", new BigDecimal(1), teaSpoonUOMOptional.get()));
+        chickenBiryani.addIngredient(new Ingredient("Coriander Powder", new BigDecimal(2), teaSpoonUOMOptional.get()));
+
+        Notes chickenBiryaniNotes = new Notes();
+        chickenBiryaniNotes.setNotes("Make this scrumptious Chicken Biryani and see the heads turning your way as the " +
+                "aroma of this delectable dish will be all over your house. Biryani, which when cooked perfectly can " +
+                "turn into a dish par excellence. Loved by all food lovers ");
+        chickenBiryani.setNotes(chickenBiryaniNotes);
+
+        chickenBiryani.getCategories().add(categoryRepository.findByCategoryname("Indian").get());
+
+        recipes.add(chickenBiryani);
         recipes.add(greenCorianderRecipe);
         return recipes;
     }
--- learnspringrecipeapp/src/test/java/tamil/learn/springframework/learnspringrecipeapp/controllers/HomeControllerTest.java
+++ learnspringrecipeapp/src/test/java/tamil/learn/springframework/learnspringrecipeapp/controllers/HomeControllerTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 import tamil.learn.springframework.learnspringrecipeapp.domain.Recipe;
 import tamil.learn.springframework.learnspringrecipeapp.services.RecipeService;
@@ -15,6 +17,9 @@ import java.util.Set;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class HomeControllerTest {
     @Mock
@@ -29,6 +34,13 @@ public class HomeControllerTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         homeController = new HomeController(recipeService);
+    }
+
+    @Test
+    public void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
+        mockMvc.perform(get("/getrecipes")).andExpect(status().isOk())
+                .andExpect(view().name("recipes"));
     }
 
     @Test
