ef53136dc77ada35da46c353792feba130e9af58
Murugan Nagarajan <murugan425@gmail.com>
2017-12-15 15:21:17 +0800

    Created this project 'learnspringinit' for Spring 5 - Initalization basics

---
+++ learnspringinit/src/main/java/tamil/learn/springframework/learnspringinit/LearnspringinitApplication.java
@@ -0,0 +1,12 @@
+package tamil.learn.springframework.learnspringinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnspringinitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnspringinitApplication.class, args);
+	}
+}
