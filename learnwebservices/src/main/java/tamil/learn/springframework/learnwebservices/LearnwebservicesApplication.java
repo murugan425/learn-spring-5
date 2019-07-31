1301ef84b55bd32c1dc81dfaa0f10bdc78eea882
Murugan Nagarajan <murugan425@gmail.com>
2019-07-31 14:52:08 +0800

    create a new project using https://start.spring.io [dependencies added - web,jpa,devtools,h2]

---
+++ learnwebservices/src/main/java/tamil/learn/springframework/learnwebservices/LearnwebservicesApplication.java
@@ -0,0 +1,13 @@
+package tamil.learn.springframework.learnwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnwebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnwebservicesApplication.class, args);
+	}
+
+}
