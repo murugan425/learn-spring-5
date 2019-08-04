8d01c2185e8ec51cd573cb83494e872ed8b71ca1
Murugan Nagarajan <murugan425@gmail.com>
2019-08-04 19:36:56 +0800

    send params in uri via requests

--- learnwebservices/src/main/java/tamil/learn/springframework/learnwebservices/controller/HelloController.java
+++ learnwebservices/src/main/java/tamil/learn/springframework/learnwebservices/controller/HelloController.java
@@ -3,6 +3,7 @@
  */
 package tamil.learn.springframework.learnwebservices.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +25,15 @@ public class HelloController {
 	@RequestMapping(method = RequestMethod.GET, path = "hellobean")
 	public HelloBean sayHelloBean() {
 		return new HelloBean("Hello Murugan - I'm a bean/pojo");
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, path = "helloparam/{param}")
+	public HelloBean sayHelloParam(@PathVariable String param) {
+		return new HelloBean("Hello "+ param);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, path = "helloparam/{param}/{param2}")
+	public HelloBean sayHelloParam(@PathVariable String param, @PathVariable String param2) {
+		return new HelloBean(String.format("Hello %s , %s", param, param2));
 	}
 }
