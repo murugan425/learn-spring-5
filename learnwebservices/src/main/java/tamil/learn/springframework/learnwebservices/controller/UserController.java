d6b0aa53ad8dbd1c11aa8300a9a0ad9b84bdcea3
ATPIAD1\atpsmnn <mnagarajan@atpco.net>
2019-10-21 08:37:31 -0400

    added produces type detail to the controller and removed the debug level.

--- learnwebservices/src/main/java/tamil/learn/springframework/learnwebservices/controller/UserController.java
+++ learnwebservices/src/main/java/tamil/learn/springframework/learnwebservices/controller/UserController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,14 +30,14 @@ public class UserController {
 		return userService.findAllUsers();
 	};
 	
-	@PostMapping(path = "users")
+	@PostMapping(path = "users", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<User> saveUser(@RequestBody User user) {
 		User newuser = userService.save(user);		
 		URI uriLoc = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newuser.getId()).toUri();
 		return ResponseEntity.created(uriLoc).build();		
 	};
 	
-	@GetMapping(path = "users/{id}")
+	@GetMapping(path = "users/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public User findUserById(@PathVariable int id) throws UserNotFoundException {
 		User user = userService.findUserById(id);
 		if(null == user) {
