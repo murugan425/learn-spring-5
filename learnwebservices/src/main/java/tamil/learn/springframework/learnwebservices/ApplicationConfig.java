bbd231057ef641813830f36462b8866f5288b4bf
Murugan Nagarajan <murugan425@gmail.com>
2019-10-22 08:35:27 -0400

    Removing the locale @session level and making use of request header locale resolver & updating the default locale using language code instead of country code.

--- learnwebservices/src/main/java/tamil/learn/springframework/learnwebservices/ApplicationConfig.java
+++ learnwebservices/src/main/java/tamil/learn/springframework/learnwebservices/ApplicationConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 /**
  * @author Murugan_Nagarajan
@@ -21,8 +21,8 @@ public class ApplicationConfig {
 			
 	@Bean
 	public LocaleResolver localResolver() {
-		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
-		localeResolver.setDefaultLocale(Locale.US);
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.ENGLISH);
 		return localeResolver;
 	}
 
