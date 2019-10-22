563da162fa4ae76f8e54e02e632a8887274718f5
Murugan Nagarajan <murugan425@gmail.com>
2019-10-22 07:02:29 -0400

    Enabling spring HATEOAS to add resource links to the response, helps user to know what they can do next base on the current response entity state.

--- learnwebservices/src/main/java/tamil/learn/springframework/learnwebservices/controller/UserController.java
+++ learnwebservices/src/main/java/tamil/learn/springframework/learnwebservices/controller/UserController.java
@@ -1,11 +1,16 @@
 package tamil.learn.springframework.learnwebservices.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import java.net.URI;
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -39,12 +44,15 @@ public class UserController {
 	};
 	
 	@GetMapping(path = "users/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public User findUserById(@PathVariable int id) throws UserNotFoundException {
+	public EntityModel<User> findUserById(@PathVariable int id) throws UserNotFoundException {
 		User user = userService.findUserById(id);
 		if(null == user) {
 			throw new UserNotFoundException("User not found for User Id = " + id);
 		}
-		return user;
+		WebMvcLinkBuilder linkToAllUsers = linkTo(methodOn(this.getClass()).findAllUsers());
+		EntityModel<User> userEntity = new EntityModel<User>(user);
+		userEntity.add(linkToAllUsers.withRel("all-users"));
+		return userEntity;
 	};
 	
 	@DeleteMapping(path = "users/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
