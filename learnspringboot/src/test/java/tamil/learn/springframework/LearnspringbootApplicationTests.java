075e5131f0ad3732e2dbcfee8abe3e224a82c275
Murugan Nagarajan <murugan425@gmail.com>
2019-11-19 23:20:13 -0500

    Created a demo spring boot project to try 2.2.2 starters. [JPA & H2 Starters added]

---
+++ learnspringboot/src/test/java/tamil/learn/springframework/LearnspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package tamil.learn.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
